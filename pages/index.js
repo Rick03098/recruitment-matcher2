86f049dcf69ad2a19cb5194a49e6e86081d15ddb
Rick03098 <18811600598@qq.com>
2025-04-15 15:41:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
+import RecruitmentMatcher from '../components/RecruitmentMatcher';
+
 export default function Home() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>招聘匹配系统</h1>
-      <p>简单版本测试</p>
-    </div>
-  );
+  return <RecruitmentMatcher />;
 }
